b235fd10480facf26eabf48e13f4aa76a30f3ff3
Jimmie Harkins <ghana796@gmail.com>
2017-11-30 16:35:07 -0500

    optimized map

--- main.js
+++ main.js
@@ -1,40 +1,58 @@
-var MAP_WIDTH = 193;
+var MAP_WIDTH = 150;
 var MAP_HEIGHT = 40;
 var TILES = ["X"," ","~"];
-var display = document.getElementById("game");
-console.log(display);
-function tile(color,character) {
-	this.color = color;
+var DISPLAY = document.getElementById("game");
+
+var MAP = [];
+		   
+var entities = [];
+
+function entity(x, y, character) { //any object that can move
+	this.x = x;
+	this.y = y;
 	this.character = character;
 }
-function draw(arrMap) {
-	display.innerHTML = "";
-	var textNode;
-	var line;
-	for(var i = 0; i < MAP_HEIGHT; i++) {
-		line = "";
-		for(var j = 0; j < MAP_WIDTH; j++) {
-			line += "<span style=\"color:" + arrMap[i][j].color + "\">" + arrMap[i][j].character + "</span>";			//add character from map array to line
-		}
-		line += "\n";
-		display.innerHTML += line;
+
+function generateMap(width, height) { //generates map of given width and height (currently random)
+	var blankSpaceForEntities = "";
+	for(var i = 0; i < width; i++) {
+		blankSpaceForEntities += " ";
 	}
-}
-var map = [];
-function generateMap() {
-	for(var i = 0; i < MAP_HEIGHT; i++) {
-		map[i] = [];
-		for(var j = 0; j < MAP_WIDTH; j++) {
-			map[i][j] = new tile("#FF0000",TILES[Math.floor(Math.random() * TILES.length)]);
+	for(var i = 0; i < height; i++) {
+		var row = "";
+		
+		for(var j = 0; j < width; j++) {
+			row += TILES[Math.floor(Math.random() * TILES.length)];
 		}
+		MAP.push(row);
+		entities.push(blankSpaceForEntities);
 	}
 }
 
 
+function draw(map, x, y) { //draws the given map at top-right position x,y
+	var output = "";
+	for(var row = y; row < y + MAP_HEIGHT; row++) { //goes down rows in view
+		//output += map[row].substring(x, x + MAP_WIDTH); // selects the right number of characters starting at the right point in the map
+		for(var column = x; column < x + MAP_WIDTH; column++) {
+			if(entities[row].substring(column, column + 1) == " ") {
+				output += map[row].substring(column,column + 1);
+			} else {
+				output += entities[row].substring(column,column + 1);
+			}
+		}
+		output += "\n";
+	}
+	DISPLAY.innerHTML = output;
+}
+
+var testy = 0;
 function gameLoop() {
-	generateMap();
-	draw(map);
+	draw(MAP, 0,testy);
+	testy++;
 }
-//draw(map);
 
-var gameInterval = setInterval(function() {gameLoop();}, 1000);
+
+
+generateMap(500,500);
+var gameInterval = setInterval(function() {gameLoop();}, 250);
