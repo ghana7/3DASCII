73aafc30a5b88ab87e24a97dc4d11f8eff265948
Jimmie Harkins <ghana796@gmail.com>
2017-11-30 23:08:45 -0500

    broken shit with players and inheritance

--- main.js
+++ main.js
@@ -2,15 +2,40 @@ var MAP_WIDTH = 150;
 var MAP_HEIGHT = 40;
 var TILES = ["X"," ","~"];
 var DISPLAY = document.getElementById("game");
+var START_X = 5;
+var START_Y = 5;
 
 var MAP = [];
 		   
 var entities = [];
+var entityMap = [];
 
-function entity(x, y, character) { //any object that can move
+function Entity(x, y, character) { //any object that can move
 	this.x = x;
 	this.y = y;
 	this.character = character;
+}
+Entity.prototype.drawIn = function(entityArray) {
+	var oldRow = entityArray[this.y];
+	entityArray[this.y] = oldRow.substring(0,this.x) + this.character + oldRow.substring(this.x + 1); //put entity in that position in that row of array
+}
+	
+	
+Player.prototype = new Entity();
+Player.prototype.constructor = Player;
+function Player() {
+	this.x = START_X;
+	this.y = START_Y;
+	this.character = "🤔";
+}
+Player.prototype.drawIn = function(entityArray) {
+	Entity.prototype.drawIn.call(this, entityArray);
+}
+
+function drawEntities(entityArray, entityMapArray) {
+	for(var i = 0; i < entityArray.length; i++) {
+		entityArray[i].drawIn(entityMapArray);
+	}
 }
 
 function generateMap(width, height) { //generates map of given width and height (currently random)
@@ -33,7 +58,6 @@ function generateMap(width, height) { //generates map of given width and height 
 function draw(map, x, y) { //draws the given map at top-right position x,y
 	var output = "";
 	for(var row = y; row < y + MAP_HEIGHT; row++) { //goes down rows in view
-		//output += map[row].substring(x, x + MAP_WIDTH); // selects the right number of characters starting at the right point in the map
 		for(var column = x; column < x + MAP_WIDTH; column++) {
 			if(entities[row].substring(column, column + 1) == " ") {
 				output += map[row].substring(column,column + 1);
@@ -48,11 +72,17 @@ function draw(map, x, y) { //draws the given map at top-right position x,y
 
 var testy = 0;
 function gameLoop() {
+	drawEntities(entities,entityMap);
 	draw(MAP, 0,testy);
+	
 	testy++;
 }
 
 
 
 generateMap(500,500);
+
+var player = new Player();
+console.log(player);
+entities.push(player);
 var gameInterval = setInterval(function() {gameLoop();}, 250);
