69805785f946132b290deb0d03db9cfd71c12e6c
Jimmie Harkins <ghana796@gmail.com>
2017-12-04 09:45:11 -0500

    almost player movement

--- main.js
+++ main.js
@@ -5,36 +5,30 @@ var DISPLAY = document.getElementById("game");
 var START_X = 5;
 var START_Y = 5;
 
+var BLANK_MAP = [];
 var MAP = [];
-		   
-var entities = [];
-var entityMap = [];
+
+var keysDown = []; //storage for keyboard monitoring
+window.onkeyup = function(e) {keysDown[e.keyCode]=false;} //creates array of booleans whether key is down or not
+window.onkeydown = function(e) {keysDown[e.keyCode]=true;}
+
+var entities = []; //list of entities
+var entityMap = []; //map-like array that is overlayed on top of map
 
 function Entity(x, y, character) { //any object that can move
 	this.x = x;
 	this.y = y;
 	this.character = character;
 }
-Entity.prototype.drawIn = function(entityArray) {
-	var oldRow = entityArray[this.y];
-	entityArray[this.y] = oldRow.substring(0,this.x) + this.character + oldRow.substring(this.x + 1); //put entity in that position in that row of array
-}
-	
-	
-Player.prototype = new Entity();
-Player.prototype.constructor = Player;
-function Player() {
-	this.x = START_X;
-	this.y = START_Y;
-	this.character = "🤔";
-}
-Player.prototype.drawIn = function(entityArray) {
-	Entity.prototype.drawIn.call(this, entityArray);
-}
 
+function drawEntity(entity,entityMapArray) {
+	var oldRow = entityMapArray[entity.y];
+	entityMapArray[entity.y] = oldRow.substring(0,entity.x) + entity.character + oldRow.substring(entity.x + 1); //put entity in that position in that row of array
+}
 function drawEntities(entityArray, entityMapArray) {
+	entityMapArray = BLANK_MAP.slice();
 	for(var i = 0; i < entityArray.length; i++) {
-		entityArray[i].drawIn(entityMapArray);
+		drawEntity(entityArray[i], entityMapArray);
 	}
 }
 
@@ -50,19 +44,22 @@ function generateMap(width, height) { //generates map of given width and height 
 			row += TILES[Math.floor(Math.random() * TILES.length)];
 		}
 		MAP.push(row);
-		entities.push(blankSpaceForEntities);
+		BLANK_MAP.push(blankSpaceForEntities);
 	}
+	entityMap = BLANK_MAP.slice();
 }
 
 
-function draw(map, x, y) { //draws the given map at top-right position x,y
+function draw(map, topLeftX, topLeftY) { //draws the given map at top-right position x,y
+	var x = Math.max(topLeftX, 0);
+	var y = Math.max(topLeftY, 0);
 	var output = "";
 	for(var row = y; row < y + MAP_HEIGHT; row++) { //goes down rows in view
 		for(var column = x; column < x + MAP_WIDTH; column++) {
-			if(entities[row].substring(column, column + 1) == " ") {
+			if(entityMap[row].substring(column, column + 1) == " ") {
 				output += map[row].substring(column,column + 1);
 			} else {
-				output += entities[row].substring(column,column + 1);
+				output += entityMap[row].substring(column,column + 1);
 			}
 		}
 		output += "\n";
@@ -70,19 +67,32 @@ function draw(map, x, y) { //draws the given map at top-right position x,y
 	DISPLAY.innerHTML = output;
 }
 
-var testy = 0;
-function gameLoop() {
-	drawEntities(entities,entityMap);
-	draw(MAP, 0,testy);
+function playerControl(playerEntity) {
 	
-	testy++;
+	if(keysDown[65]) { //A
+		playerEntity.x--;
+	}
+	if(keysDown[68]) { //D
+		playerEntity.x++;
+	}
+	if(keysDown[87]) { //W
+		playerEntity.y--;
+	}
+	if(keysDown[83]) { //S
+		playerEntity.y++;
+	}
 }
-
-
 
 generateMap(500,500);
 
-var player = new Player();
-console.log(player);
+var player = new Entity(START_X, START_Y, "B");
 entities.push(player);
+
+
+function gameLoop() {
+	playerControl(player);
+	drawEntities(entities,entityMap);
+	draw(MAP, player.x - Math.floor(MAP_WIDTH / 2), player.y - Math.floor(MAP_HEIGHT / 2));
+}
+
 var gameInterval = setInterval(function() {gameLoop();}, 250);
